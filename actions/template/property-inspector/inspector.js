13a5c67b1b9af640309a6d3b6f1c03be6a869828
Justin Mayfield <tooker@gmail.com>
2022-12-28 16:13:56 -0800

    init

---
+++ actions/template/property-inspector/inspector.js
@@ -0,0 +1,33 @@
+/// <reference path="../../../libs/js/property-inspector.js" />
+/// <reference path="../../../libs/js/utils.js" />
+
+$PI.onConnected((jsn) => {
+	$PI.loadLocalization('../../../');
+
+	const form = document.querySelector('#property-inspector');
+	const { actionInfo, appInfo, connection, messageType, port, uuid } = jsn;
+	const { payload, context } = actionInfo;
+	const { settings } = payload;
+
+	Utils.setFormValue(settings, form);
+
+	form.addEventListener(
+		'input',
+		Utils.debounce(150, () => {
+			const value = Utils.getFormValue(form);
+			$PI.setSettings(value);
+		})
+	);
+});
+
+/**
+ * Provide window level functions to use in the external window
+ * (this can be removed if the external window is not used)
+ */
+window.sendToInspector = (data) => {
+	console.log(data);
+};
+
+document.querySelector('#open-external').addEventListener('click', () => {
+	window.open('../../../external.html');
+});
